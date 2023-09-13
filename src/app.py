3fb52d33d60bb609d9dec69a601f33ea2c5f36d0
kvruntime <kvruntime@gmail.com>
2023-09-13 16:45:54 +0000

    update code

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import sys
 from di_builder import di
 
 from pages import HomePage
@@ -12,22 +13,14 @@ di.add_singleton(HomePage)
 class Application(QApplication):
     def __init__(self) -> None:
         super().__init__([])
-        # self.register()
-
         self.page = di.resolve(HomePage)
         return
 
-    def register(self):
-        di.add_singleton(HomePage)
-        di.add_singleton(HomeViewModel)
-        di.add_singleton(DataService)
-        di.add_singleton(self.__class__)
-        return None
 
     def run(self) -> None:
         self.page.show()
-        self.exec()
-        return
+        sys.exit(self.exec())
+
 
 
 di.add_singleton(Application)
